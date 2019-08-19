018cdc035a03f06c460b46f53495d5de8a90a2e0
grumpington <gaminggrump@gmail.com>
2019-08-19 19:13:40 +0200

    first version

---
+++ dingshot.lua
@@ -0,0 +1,20 @@
+local totalplayed = 0
+local dingshotframe = CreateFrame("FRAME", "dingshot", UIParent)
+
+local function dingshotEvent1()
+	C_Timer.After(2, function() dingshotEvent2() end)
+end
+
+function dingshotEvent2()
+	RequestTimePlayed()
+	print ("Current level: " .. UnitLevel("player"))
+	--this is for verification of xp per level for another addon
+	--print ("XP needed for next level: " .. UnitXPMax("player"))
+	C_Timer.After(2, function() Screenshot() end)
+	return nil
+end
+
+
+dingshotframe:RegisterEvent("PLAYER_LEVEL_UP")
+--dingshotframe:RegisterEvent("PLAYER_ENTERING_WORLD")
+dingshotframe:SetScript("OnEvent", dingshotEvent1)
