d7dbbce0c4c0eafbca6079397ac187ec4e874e03
grumpington <gaminggrump@gmail.com>
2019-08-22 23:23:27 +0200

    fixed a bug

--- dingshot.lua
+++ dingshot.lua
@@ -9,12 +9,12 @@ function dingshotEvent2()
 	RequestTimePlayed()
 	print (L["currentlevel"] .. UnitLevel("player"))
 	--this is for verification of xp per level for another addon
-	--print ("XP needed for next level: " .. UnitXPMax("player"))
+	print ("XP needed for next level: " .. UnitXPMax("player"))
 	C_Timer.After(2, function() Screenshot() end)
 	return nil
 end
 
 
 dingshotframe:RegisterEvent("PLAYER_LEVEL_UP")
-dingshotframe:RegisterEvent("PLAYER_ENTERING_WORLD")
+--dingshotframe:RegisterEvent("PLAYER_ENTERING_WORLD")
 dingshotframe:SetScript("OnEvent", dingshotEvent1)
